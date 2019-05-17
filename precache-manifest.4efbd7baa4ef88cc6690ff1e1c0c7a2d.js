743f5439eac65b194fc4edbd5cb361d7bbe253ce
yosuahohoho <yosuahohoho@gmail.com>
2019-05-17 14:20:24 +0700

    Updates

---
+++ precache-manifest.4efbd7baa4ef88cc6690ff1e1c0c7a2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a936d1780de7ccc53c3493b7d47e415",
+    "url": "/seneca-words-react/index.html"
+  },
+  {
+    "revision": "d8f82208e48c49087191",
+    "url": "/seneca-words-react/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "dc19f2c2d38c4f83333f",
+    "url": "/seneca-words-react/static/css/main.488d27d4.chunk.css"
+  },
+  {
+    "revision": "d8f82208e48c49087191",
+    "url": "/seneca-words-react/static/js/2.8bc77389.chunk.js"
+  },
+  {
+    "revision": "dc19f2c2d38c4f83333f",
+    "url": "/seneca-words-react/static/js/main.cef73f63.chunk.js"
+  },
+  {
+    "revision": "39c54dc7f6ee8a0928cb",
+    "url": "/seneca-words-react/static/js/runtime~main.4d0a040e.js"
+  }
+]);
