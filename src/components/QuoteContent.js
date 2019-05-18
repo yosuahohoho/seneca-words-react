beefac284f323231499e19c2dc2cb44928c4e022
yosuahohoho <yosuahohoho@gmail.com>
2019-05-18 17:25:23 +0700

    delete: react-pose

--- src/components/QuoteContent.js
+++ src/components/QuoteContent.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import posed, { PoseGroup } from 'react-pose';
 
-const QuotePose = posed.div({
-    enter: {
-        opacity: 1
-    },
-    exit: {
-        opacity: 0
-    }
-})
+
 
 const QuoteContent = (props) => {
     return (
-        <PoseGroup>
-            {props.loaded && [
-                <QuotePose key="quote">
-                    <p className="quote__text">{props.quote.message}</p>
-                    <p className="quote__author">- {props.quote.author}</p>
-                </QuotePose>
-            ]}
-        </PoseGroup>
+        <section>
+            <p className="quote__text">{props.quote.message}</p>
+            <p className="quote__author">- {props.quote.author}</p>
+        </section>
+
     )
 }
 
