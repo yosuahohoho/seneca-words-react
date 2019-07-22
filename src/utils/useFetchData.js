cad630f33e4a94497d0938765599218656efa89c
yosuahohoho <yosuahohoho@gmail.com>
2019-07-22 12:57:10 +0700

    use useFetchData custom hook

--- src/utils/useFetchData.js
+++ src/utils/useFetchData.js
@@ -2,28 +2,30 @@ import { useState, useEffect } from 'react';
 
 const useFetchData = (url) => {
   const [data, setData] = useState('')
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(true)
   const [isError, setIsError] = useState(false)
 
-  useEffect(() => {
-    const fetchData = async () => {
-      setIsLoading(true)
-
-      try {
-        const result = await fetch(url)
-        setData(result.data)
-      }
-      catch(e) {
-        setIsError(true)
-      }
+  const fetchData = async () => {
+    setIsLoading(true)
+    try {
+      const response = await fetch(url)
+      const result = await response.json()
       
+      setIsLoading(false)
+      setData(result)
     }
-
+    catch(e) {
+      setIsLoading(false)
+      setIsError(true)
+    }
+  }
+  
+  // eslint-disable-next-line
+  useEffect(() => {
     fetchData()
-
-    return [{ data, isLoading, isError }, fetchData]
-
   }, [])
+
+  return [{ data, isLoading, isError }, fetchData]
 
 }
 
