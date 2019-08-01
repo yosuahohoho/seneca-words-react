6e57c674ce9a5d31580c997de88d6c74f1cfbce6
yosuahohoho <yosuahohoho@gmail.com>
2019-08-01 17:08:39 +0700

    formated code

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from 'react'
 
 const Header = () => {
-    return (
-        <header>
-            <h1>Seneca Words</h1>
-        </header>
-    )
+  return (
+    <header>
+      <h1>Seneca Words</h1>
+    </header>
+  )
 }
 
-export default Header;
+export default Header
