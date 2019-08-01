61d23b733dc6bbaec2e8559f0aba9c3f5a03dae4
yosuahohoho <yosuahohoho@gmail.com>
2019-08-01 11:16:25 +0700

    Updates

--- precache-manifest.013dc94f810d2929c87dcd2d448c3246.js
+++ precache-manifest.013dc94f810d2929c87dcd2d448c3246.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0813d7d1787d81b9d4dc3c3578b2ea54",
+    "revision": "c7eea4a79d871a07a264c7533d5f02a7",
     "url": "/seneca-words-react/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/seneca-words-react/static/css/2.764ccc25.chunk.css"
   },
   {
-    "revision": "5d7cccc194696002e22f",
-    "url": "/seneca-words-react/static/css/main.3e57eb6d.chunk.css"
+    "revision": "1e24c552647fe497f9aa",
+    "url": "/seneca-words-react/static/css/main.28e4cfeb.chunk.css"
   },
   {
     "revision": "34c04d7a90e9c55131bc",
     "url": "/seneca-words-react/static/js/2.5b9f15f4.chunk.js"
   },
   {
-    "revision": "5d7cccc194696002e22f",
-    "url": "/seneca-words-react/static/js/main.fc38843e.chunk.js"
+    "revision": "1e24c552647fe497f9aa",
+    "url": "/seneca-words-react/static/js/main.ad4369fe.chunk.js"
   },
   {
     "revision": "39c54dc7f6ee8a0928cb",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/seneca-words-react/precache-manifest.467686f55e7db158fc00917b89561d34.js"
+  "/seneca-words-react/precache-manifest.013dc94f810d2929c87dcd2d448c3246.js"
 );
 
 self.addEventListener('message', (event) => {
